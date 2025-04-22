09e33fe8b1b4e98c8a3884f662bd5b80e69b4e14
anindita anggraini putri <117986825+anindta@users.noreply.github.com>
2025-04-22 13:55:10 +0700

    pertemuan ke 6

--- database/migrations/2025_04_22_114635_create_order_table.php
+++ database/migrations/2025_04_22_114635_create_order_table.php
@@ -11,17 +11,31 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('order', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        Schema::create('order', function (Blueprint $table) { 
+            $table->id(); 
+            $table->unsignedBigInteger('customer_id'); 
+            $table->unsignedBigInteger('user_id')->nullable(); 
+            $table->string('status'); 
+            $table->string('noresi')->nullable(); 
+            $table->string('kurir')->nullable(); 
+            $table->string('layanan_ongkir')->nullable(); 
+            $table->string('biaya_ongkir')->nullable(); 
+            $table->string('estimasi_ongkir')->nullable(); 
+            $table->integer('total_berat')->nullable(); 
+            $table->double('total_harga'); 
+            $table->text('alamat')->nullable(); 
+            $table->string('pos')->nullable(); 
+            $table->timestamps(); 
+            $table->foreign('customer_id')->references('id')->on('customer')->onDelete('cascade'); 
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade'); 
+        }); 
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::dropIfExists('order');
-    }
+    // public function down(): void
+    // {
+    //     Schema::dropIfExists('order');
+    // }
 };
--- routes/web.php
+++ routes/web.php
@@ -1,52 +1,59 @@
-<?php 
-use Illuminate\Support\Facades\Route; 
-use App\Http\Controllers\BerandaController; 
-use App\Http\Controllers\LoginController; 
-use App\Http\Controllers\UserController; 
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\ProdukController; 
-use App\Http\Controllers\CustomerController; 
+use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\OrderController;
 
-Route::get('/', function () { 
-// return view('welcome'); 
+Route::get('/', function () {
+    // return view('welcome'); 
 // return redirect()->route('backend.login'); 
-return redirect()->route('beranda');
-}); 
-Route::get('backend/beranda', [BerandaController::class, 'berandaBackend'])->name('backend.beranda')->middleware('auth'); 
-Route::get('backend/login', [LoginController::class, 'loginBackend'])->name('backend.login'); 
-Route::post('backend/login', [LoginController::class, 'authenticateBackend'])->name('backend.login'); 
-Route::post('backend/logout', [LoginController::class, 'logoutBackend'])->name('backend.logout'); 
-Route::resource('backend/user', UserController::class, ['as' => 'backend'])->middleware('auth'); 
-Route::resource('backend/kategori', KategoriController::class, ['as' => 'backend'])->middleware('auth'); 
+    return redirect()->route('beranda');
+});
+Route::get('backend/beranda', [BerandaController::class, 'berandaBackend'])->name('backend.beranda')->middleware('auth');
+Route::get('backend/login', [LoginController::class, 'loginBackend'])->name('backend.login');
+Route::post('backend/login', [LoginController::class, 'authenticateBackend'])->name('backend.login');
+Route::post('backend/logout', [LoginController::class, 'logoutBackend'])->name('backend.logout');
+Route::resource('backend/user', UserController::class, ['as' => 'backend'])->middleware('auth');
+Route::resource('backend/kategori', KategoriController::class, ['as' => 'backend'])->middleware('auth');
 Route::resource('backend/produk', ProdukController::class, ['as' => 'backend'])->middleware('auth');
-Route::post('foto-produk/store', [ProdukController::class, 'storeFoto'])->name('backend.foto_produk.store')->middleware('auth'); 
-Route::resource('backend/customer', CustomerController::class, ['as' => 'backend'])->middleware('auth'); 
+Route::post('foto-produk/store', [ProdukController::class, 'storeFoto'])->name('backend.foto_produk.store')->middleware('auth');
+Route::resource('backend/customer', CustomerController::class, ['as' => 'backend'])->middleware('auth');
 // Route untuk menghapus foto 
-Route::delete('foto-produk/{id}', [ProdukController::class, 'destroyFoto'])->name('backend.foto_produk.destroy')->middleware('auth'); 
-Route::get('backend/laporan/formuser', [UserController::class, 'formUser'])->name('backend.laporan.formuser')->middleware('auth'); 
+Route::delete('foto-produk/{id}', [ProdukController::class, 'destroyFoto'])->name('backend.foto_produk.destroy')->middleware('auth');
+Route::get('backend/laporan/formuser', [UserController::class, 'formUser'])->name('backend.laporan.formuser')->middleware('auth');
 Route::post('backend/laporan/cetakuser', [UserController::class, 'cetakUser'])->name('backend.laporan.cetakuser')->middleware('auth');
-Route::get('backend/laporan/formproduk', [ProdukController::class, 'formProduk'])->name('backend.laporan.formproduk')->middleware('auth'); 
-Route::post('backend/laporan/cetakproduk', [ProdukController::class, 'cetakProduk'])->name('backend.laporan.cetakproduk')->middleware('auth'); 
+Route::get('backend/laporan/formproduk', [ProdukController::class, 'formProduk'])->name('backend.laporan.formproduk')->middleware('auth');
+Route::post('backend/laporan/cetakproduk', [ProdukController::class, 'cetakProduk'])->name('backend.laporan.cetakproduk')->middleware('auth');
 // Frontend 
-Route::get('/beranda', [BerandaController::class, 'index'])->name('beranda'); 
-Route::get('/produk/detail/{id}', [ProdukController::class, 'detail'])->name('produk.detail'); 
-Route::get('/produk/kategori/{id}', [ProdukController::class, 'produkKategori'])->name('produk.kategori'); 
-Route::get('/produk/all', [ProdukController::class, 'produkAll'])->name('produk.all'); 
+Route::get('/beranda', [BerandaController::class, 'index'])->name('beranda');
+Route::get('/produk/detail/{id}', [ProdukController::class, 'detail'])->name('produk.detail');
+Route::get('/produk/kategori/{id}', [ProdukController::class, 'produkKategori'])->name('produk.kategori');
+Route::get('/produk/all', [ProdukController::class, 'produkAll'])->name('produk.all');
 
 //API Google 
-Route::get('/auth/redirect', [CustomerController::class, 'redirect'])->name('auth.redirect'); 
-Route::get('/auth/google/callback', [CustomerController::class, 'callback'])->name('auth.callback'); 
+Route::get('/auth/redirect', [CustomerController::class, 'redirect'])->name('auth.redirect');
+Route::get('/auth/google/callback', [CustomerController::class, 'callback'])->name('auth.callback');
 
 // Route untuk menampilkan halaman akun customer 
 // Route::get('/customer/akun/{id}', [CustomerController::class, 'akun'])->name('customer.akun')->middleware('is.customer'); 
 // Route::put('/customer/akun/{id}/update', [CustomerController::class, 'updateAkun'])->name('customer.akun.update')->middleware('is.customer'); 
 
 // Group route untuk customer 
-Route::middleware('is.customer')->group(function () { 
+Route::middleware('is.customer')->group(function () {
     // Route untuk menampilkan halaman akun customer 
-    Route::get('/customer/akun/{id}', [CustomerController::class, 'akun']) ->name('customer.akun'); 
+    Route::get('/customer/akun/{id}', [CustomerController::class, 'akun'])->name('customer.akun');
     // Route untuk mengupdate data akun customer 
-    Route::put('/customer/updateakun/{id}', [CustomerController::class, 'updateAkun']) ->name('customer.updateakun'); 
-    }); 
+    Route::put('/customer/updateakun/{id}', [CustomerController::class, 'updateAkun'])->name('customer.updateakun');
+    // Route untuk menambahkan produk ke keranjang 
+    Route::post('add-to-cart/{id}', [OrderController::class, 'addToCart'])->name('order.addToCart');
+    Route::get('cart', [OrderController::class, 'viewCart'])->name('order.cart');
+});
+
+
 // Logout 
 Route::post('/logout', [CustomerController::class, 'logout'])->name('customer.logout');
+
